07a1149be25595617177e39b57e06a5f0f3b272a
digitalh2o <digitalh2o@gmail.com>
2018-05-20 15:29:29 -0500

    Wiring up reducer for Employee Form Component

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
-import { Router, Scene } from "react-native-router-flux";
+import { Router, Scene, Actions } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
 import EmployeeList from "./components/EmployeeList";
+import EmployeeCreate from "./components/EmployeeCreate";
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Please Login" />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      <Scene key="root" hideNavBar>
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please Login" />
+        </Scene>
+
+        <Scene key="main">
+          <Scene
+            rightTitle="Add"
+            onRight={() => {
+              Actions.employeeCreate();
+            }}
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
+        </Scene>
       </Scene>
     </Router>
   );
